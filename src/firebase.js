f7771d4111f8d7e26fb61dfc4353035a9149996f
BangAldy21 <anggachannel011@gmail.com>
2025-08-25 21:19:02 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,28 @@
-// TODO: paste your firebase.js config here (yang sudah kamu punya).
-// Example:
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// const firebaseConfig = { /* ...punyamu... */ };
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// const provider = new GoogleAuthProvider();
-// export const loginWithGoogle = () => signInWithPopup(auth, provider);
-// export const logout = () => signOut(auth);
-// export const db = getFirestore(app);
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { 
+  getAuth, GoogleAuthProvider, signInWithPopup, signOut 
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWPzULJKa7ApK7psIpr38_iChCZM-_dxQ",
+  authDomain: "bangaldy21-51f0b.firebaseapp.com",
+  projectId: "bangaldy21-51f0b",
+  storageBucket: "bangaldy21-51f0b.firebasestorage.app",
+  messagingSenderId: "528551198877",
+  appId: "1:528551198877:web:3aec52590ebdbd26159474",
+  measurementId: "G-H3T9358YWH"
+};
+
+// Init app
+const app = initializeApp(firebaseConfig);
+
+// Auth setup
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export const loginWithGoogle = () => signInWithPopup(auth, provider);
+export const logout = () => signOut(auth);
+
+// Firestore
+export const db = getFirestore(app);
